e6c588939b71006f08b1d130b238996081566aaa
Tom <tomhazredstone@hotmail.com>
2018-12-02 20:23:45 -0600

    Delete unintentional test server upload

--- bukkit/test/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Xms3G -Xmx3G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -Dusing.aikars.flags=mcflags.emc.gs -jar paperclip.jar
